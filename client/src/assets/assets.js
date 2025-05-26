fe3770a245913162566640b68be0b2861b71c88c
Koushik Guria <koushikguria998@gmail.com>
2025-05-26 22:05:27 +0530

    Result page features added

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -11,6 +11,7 @@ import image_wo_bg from './image_wo_bg.png'
 import facebook_icon from './facebook_icon.svg'
 import google_plus_icon from './google_plus_icon.svg'
 import twitter_icon from './twitter_icon.svg'
+import x_icon from './x_icon.svg'
 import profile_img_1 from './profile_img_1.png'
 import profile_img_2 from './profile_img_2.png'
 import credit_icon from './credit_icon.png'
@@ -28,6 +29,7 @@ export const assets = {
     facebook_icon,
     google_plus_icon,
     twitter_icon,
+    x_icon,
     upload_btn_icon,
     credit_icon
 }
